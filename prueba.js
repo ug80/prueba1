bdf694e75aa4a7eeb957af702fa4feddeb7f08c0
Ulises Giordano <giordau@gmail.com>
2023-10-10 12:25:18 -0300

    comit de prueba 1

---
+++ prueba.js
@@ -0,0 +1,5 @@
+const weight  = "12 - 15";
+
+const pesomin = parseInt(weight.split('-')[1]);
+
+console.log(pesomin)
---
+++ prueba2.js
@@ -0,0 +1,5 @@
+const array = [3,4,6,1,3,]
+
+const sum = array.reduce((acc, el)=> acc+ el);
+
+console.log(sum)
